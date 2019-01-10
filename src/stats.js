e060055b46469c549c576b635781ebe92bf5fa15
Nemferno <jasperbae@gmail.com>
2019-01-09 19:50:06 -0500

    include stats

--- src/stats.js
+++ src/stats.js
@@ -1,4 +1,13 @@
 
+function StatModule() {}
 
+/**
+ * Gives a logic result if the bus is on break
+ * @returns {boolean}
+ */
+StatModule.isOnBreak = function(duration) {
+    const probability = -0.077820309+0.005038471 * duration;
+    return probability >= 0.7;
+};
 
 module.exports = { StatModule };
